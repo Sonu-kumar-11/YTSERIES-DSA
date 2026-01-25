5cba5de745af756225487eefcc871d40c0145b39
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-01-25 16:58:42 +0530

    Update code

--- code.cpp
+++ code.cpp
@@ -6,7 +6,7 @@ int main() {
     cout<<"enter char :";
     cin >> ch;
 
-    if(ch >='a' && ch <= 'z') {
+    if(ch <='a' && ch >= 'z') {
         cout << "lowercase\n";
     } else {
         cout << "uppercase\n";
