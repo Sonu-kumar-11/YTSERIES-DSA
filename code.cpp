e0aa23f6efd9c15c3e105f0b4c9d08d887ef0518
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-02-02 19:49:11 +0530

    added pattern dsa programs

--- code.cpp
+++ code.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 
 int main() {
-char ch = 'A';
-int n =3;
-for(int i=0;i < 3; i++){
-for(int j=0; j< n; j++){
-    cout << ch << " ";
-    ch++;
+int n=5;
+
+for (int i=0;i<n;i++){
+    for (int j=0;j<i+1;j++) {
+        cout << " * ";
+    }
+    cout << endl;
 }
-cout << endl;
-}
-cout << "after pattern :" << ch << endl;
+
   return 0;
 }
