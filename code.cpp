41f80895672666088947710ea18a2f1d2527fc7c
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-02-04 17:00:44 +0530

    initial commit

--- code.cpp
+++ code.cpp
@@ -6,8 +6,8 @@ int main() {
 
 int n=5;
 for (int i=0;i<n;i++){
-    for (int j=0;j<i+1;j++) {
-        cout << ch << " ";
+    for (int j=i+1;j>0;j--) {
+        cout << j << " ";
     }
     cout << endl;
     ch++;
