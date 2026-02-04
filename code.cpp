81f311e35fdea46512936a6320f14c8d501f38f6
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-02-04 17:29:38 +0530

    update code.cpp

--- code.cpp
+++ code.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int main() {
-    char ch ='A';
+  char ch ='A';
 
 int n=5;
 for (int i=0;i<n;i++){
-    for (int j=i+1;j>0;j--) {
-        cout << j << " ";
+    for (int j=i+1; j>0;j--) {
+        cout << ch << " ";
+        ch++;
     }
     cout << endl;
-    ch++;
+ 
 }
 
   return 0;
