46146e05cadc319848ae9138fb38f96840a1b1d8
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-02-05 16:35:05 +0530

    update code.cpp

--- code.cpp
+++ code.cpp
@@ -2,17 +2,39 @@
 using namespace std;
 
 int main() {
-  char ch ='A';
+int n=4;
 
-int n=5;
-for (int i=0;i<n;i++){
-    for (int j=i+1; j>0;j--) {
-        cout << ch << " ";
-        ch++;
-    }
-    cout << endl;
- 
+for(int i=0;i<n;i++){
+   //spaces
+ for (int j=0;j<n-i-1;j++){
+    cout << " ";
+ }
+ cout << "*";
+
+ if(i !=0){
+   //spaces
+ for(int j=0;j<2*i-1;j++){
+   cout << " ";
+ } 
+ cout << "*";
+ }
+   cout << endl;
 }
 
+//bottom
+ for (int i=0;i<n-2;i++){
+   for(int j=0; j<i+1;j++){
+      cout << " ";
+   }
+    cout << "*";
+
+    if(i!=n-1){
+      for(int j=0;j<2*(n-i)-5;j++){
+         cout << " ";
+      }
+      cout << "*";
+    }
+cout << endl;
   return 0;
+   }
 }
