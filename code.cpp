bd05b27280c4a13320ab6e0bd0a14e078159f097
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-02-16 12:48:21 +0530

    update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,34 +1,30 @@
 #include <iostream>
 using namespace std;
-int decTOBinary(int decNum){
-  int ans=0, pow=1;
 
-  while(decNum > 0){
-    int rem =decNum % 2;
-    decNum /=2;
-    ans += (rem*pow);
-    pow*=10;
-  }
-   return ans; //binary form
-   
-}
-
-int binToDecimal(int binNum){
-  int ans = 0, pow=1;
-
-  while(binNum>0){
-    int rem = binNum%10;
-    ans += rem * pow;
-
-    binNum/=10;
-    pow*=2;
-  }
-  return ans; //decimal form
-
-}
 int main(){
-cout << binToDecimal(10110) << endl;
+  int side;//side of a square
+  cin >>side;
+  cout<<"area="<<side*side<<endl;
+
+float pencost,pencilcost,erasercost;//total cost
+cin>>pencost;
+cin>>pencilcost;
+cin>>erasercost;
+
+float totalcost=pencost+pencilcost+erasercost;
+
+cout<<"total="<<totalcost<<endl;
+cout<<"totalwith gst="<<(totalcost+(0.18*totalcost))<<endl;
+
+float P,R,T;//prt
+cin>>P;
+cin>>R;
+cin>>T;
+cout<<"SI="<<(P*R*T)/100<<endl;
+
+float rad;//radius
+cin>>rad;
+cout<<"area="<<(22/7*rad*rad)<<endl;
+
 return 0;
 }
-
-  
