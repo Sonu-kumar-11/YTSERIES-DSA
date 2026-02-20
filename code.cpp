a91b224127ce92a568a71aae6e432a14ce02d888
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-02-21 00:34:17 +0530

    butterfly

--- code.cpp
+++ code.cpp
@@ -4,16 +4,39 @@ using namespace std;
 
 
 int main(){
-int n = 10;
-int first=0,sec=1;
-cout<<first<<" "<<sec<<" "; 
-for(int i=2;i<n;i++){
-    int third=first+sec;
-    cout<<third<<" ";
-    first=sec;
-    sec=third;
+int n=6;
+//star
+for(int i=1;i<=n;i++){
+for(int j=1;j<=i;j++){
+    cout<<"*";
+}//spaces
+for(int j=1;j<=2*(n-i);j++) {
+    cout<<" ";
 }
-cout<<"\n";
 
-return 0;
+//stars 
+for(int j=1;j<=i;j++){
+    cout<<"*";
 }
+cout<<endl;
+}
+
+for(int i=n;i>=1;i--){
+for(int j=1;j<=i;j++){
+    cout<<"*";
+}//spaces
+for(int j=1;j<=2*(n-i);j++) {
+    cout<<" ";
+}
+
+//stars 
+for(int j=1;j<=i;j++){
+    cout<<"*";
+}
+cout<<endl;
+}
+return 0;
+
+    }
+
+  
