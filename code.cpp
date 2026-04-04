e402eef7b4bd10059779233e6b9f25be5b9dc4d5
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-04-04 10:42:55 +0530

    pointer

--- code.cpp
+++ code.cpp
@@ -2,27 +2,13 @@
 #include <iostream>
 using namespace std;
 
- int binsearch(int *arr, int n,int key){
-    int st=0, end = n-1;
-
-    while(st<=end){
-        int mid=(st+end)/2;
-        if(arr[mid]==key){
-            return mid ;//key
-        }else if(arr[mid]<key){//2nd half
-          st=mid+1;
-        }else{// 1st half
-            end =mid-1;
-        }
-    }
-    return -1;
-
- }
-
-int main() {
-        int arr[]={2,4,6,8,10,12,14,16};
-        int n= sizeof (arr)/sizeof(int);
-    cout<< binsearch(arr,n,12);
-
+ int main(){
+    int a =10; 
+    int *ptr = &a; 
+     cout<<ptr<<"\n";//1st
+    cout<<*ptr<<"\n";
+   
+    *ptr=20;
+    cout <<a<<"\n";
 return 0;
 }
