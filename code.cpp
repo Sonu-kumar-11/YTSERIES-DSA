40cbfee638751d0c68947b9a124fb7b105123833
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-04-05 11:33:48 +0530

    kadane array

--- code.cpp
+++ code.cpp
@@ -4,27 +4,36 @@
 #include<algorithm>
 using namespace std;
 
-void maxsubarraysum1(int *arr,int n){
+void maxsubarraysum2(int *arr,int n){
 int maxsum = INT_MIN ;
 
-    for(int start=0;start<n;start++){
-for(int end=start;end<n;end++){
-    int currsum=0;
-    for(int i=start;i<=end;i++){
-        currsum+=arr[i];
-    }
-    cout<< currsum<<",";
+    for(int start=0;start<n;start++){//start=2
+        int currsum=0;
+for(int end=start;end<n;end++){// end 2,3,4,
+   currsum += arr[end];
     maxsum= max(maxsum,currsum);
     
     }
     cout<< endl;
 }
-cout<< "maximum subarray sum ="<< maxsum<< endl;
+cout << "maximum subarray sum="<<maxsum<< endl;
 }
-int main(){
-    int arr[6]={2,-3,6,-5,4,2};
+void maxsubarraysum3(int *arr,int n){
+int maxsum = INT_MIN ;
+int currsum=0;
+for(int i=0;i<n;i++){
+    currsum += arr[i];
+    maxsum=max(currsum,maxsum);
+    if(currsum<0){
+        currsum=0;
+    }
+   }
+      cout << "maximum subarray sum ="<< maxsum << endl;
+}
+int main() {
+    int arr[4]={-2,-3,-6,-5,};
     int n=sizeof(arr) / sizeof(int);
    
-maxsubarraysum1(arr,n);
+maxsubarraysum3(arr,n);
 return 0;
 }
