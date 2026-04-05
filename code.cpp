21cf38785be1e36775353c7e51a867b916cfa2ba
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-04-05 17:36:17 +0530

    bubble sort

--- code.cpp
+++ code.cpp
@@ -3,30 +3,36 @@
 #include<climits>
 #include<algorithm>
 using namespace std;
+int trap(int *heights, int n){
+    int leftmax[20000] ,rightmax[20000];
+    leftmax[0]=heights[0];
+    rightmax[n-1]=heights[n-1];
+   
 
-void maxprofit(int *prices ,int n){
-    int bestBuy[100000];
-    bestBuy[0]=INT_MAX;
-
-    for(int i=1;i<n;i++){
-        bestBuy[i]=min(bestBuy[i-1],prices[i-1]);
-       
+    for(int i=1; i<n; i++){
+        leftmax[i]= max(leftmax[i-1],heights[i-1]);
+      
     }
-    int maxprofit =0;
-    for (int i=0;i<n;i++){
-        int currprofit = prices[i] - bestBuy[i];
-        maxprofit = max(maxprofit ,currprofit);
-    }
-    //o(n+n)=o(2n)=o(n)
-    cout<<"max profit ="<< maxprofit << endl;
+    for(int i=n-2; i>0; i--){
+        rightmax[i]= max(rightmax[i+1],heights[i+1]);
+  
+}
+int watertrapped=0;
+for(int i=0; i<n; i++){
+   int currwater= min(leftmax[i],rightmax[i]-heights[i]) ;
+   if (currwater>0){
+    watertrapped += currwater;
+   }
 }
 
-
+ cout << "water trapped=" << watertrapped << endl;
+ return watertrapped;
+}
 int main() {
-    int prices[6]={7,1,5,3,6,4};
-    int n=sizeof(prices) / sizeof(int);
+    int heights[7]={4,2,0,6,3,2,5};
+    int n=sizeof(heights) / sizeof(int);
    
-    maxprofit(prices,n);
+   trap(heights,n);
 
 return 0;
 }
