66081450da240456830649f94d5850b3f976c8db
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-04-06 18:57:30 +0530

    counting sort

--- code.cpp
+++ code.cpp
@@ -1,23 +1,41 @@
 #include<iostream>
 using namespace std;
+#include<climits>
+#include<algorithm>
 
-void insertionsort(int arr[], int n) {
-    for(int i=0;i<n;i++){
-int curr =arr[i];
-int prev =i-1;
-while(prev>=0 && arr[prev] < curr){
-    swap(arr[prev],arr[prev+1]);
-    prev--;
+void countsort(int arr[], int n) {
+   
+int freq [100000] ={0};//range
+int minval = INT_MAX, maxval= INT_MIN;
+ for(int i=0;i<n;i++){
+    minval =min(minval, arr[i]);
+     maxval =max(maxval, arr[i]);
 }
-arr[prev+1]=curr;  
-}
+
+// 1st step
+for(int i=0 ; i<n; i++ ){
+    freq[arr[i]]++;
     
 }
+//2nd step  -0(range)= max-min 
+for(int i= minval,j=0; i<= maxval ; i++){
+    while(freq[i]>0){
+        arr[j++]= i;
+        freq[i]--;
+    }
+ 
+   
+}
+
+}
 int main(){
-    int arr[5]= {5,4,1,3,2};
-insertionsort(arr,5);
-for(int i=0;i<5;i++){
-    cout<<arr[i]<<" ";
+    int arr[8]= {1,4,1,3,2,4,3,7};
+countsort(arr,8);
+int n=8;
+countsort(arr,n);
+cout<<"sorted array:";
+for (int i=0; i<n; i++){
+    cout << arr[i]<< " ";
 }
 
     return 0;
