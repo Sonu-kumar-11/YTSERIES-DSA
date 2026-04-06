12cdf83fd3211a7bb5565491bdf1dd600a40075c
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-04-06 17:42:09 +0530

    insertion sort

--- code.cpp
+++ code.cpp
@@ -1,20 +1,24 @@
 #include<iostream>
 using namespace std;
 
-void selectionsort(int arr[], int n) {
-    for(int i=0;i<n-1;i++){
-        int minIDX=i;
-        //loop for find min
-        for(int j=i+1; j<n; j++){
-            if (arr[j]>arr[minIDX]){
-                minIDX=j;
-            }
-        }
-
-    }
+void insertionsort(int arr[], int n) {
+    for(int i=0;i<n;i++){
+int curr =arr[i];
+int prev =i-1;
+while(prev>=0 && arr[prev] < curr){
+    swap(arr[prev],arr[prev+1]);
+    prev--;
+}
+arr[prev+1]=curr;  
+}
+    
 }
 int main(){
     int arr[5]= {5,4,1,3,2};
+insertionsort(arr,5);
+for(int i=0;i<5;i++){
+    cout<<arr[i]<<" ";
+}
 
     return 0;
 }
