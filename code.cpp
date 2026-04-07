58d37d2d8d860713a77e468c79ecde1daf3a4dfe
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-04-07 11:07:05 +0530

    spiral matrix

--- code.cpp
+++ code.cpp
@@ -2,24 +2,42 @@
 using namespace std;
 #include<climits>
 #include<algorithm>
+ void spiralmatrix( int mat[][4], int n, int m ){
+    int srow =0, scol=0;
+    int erow=n-1, ecol=m-1;
+while(srow<= erow && scol <=ecol ) { //odd matrix
+    //top
+    for(int j=scol;j<=ecol;j++){
+        cout << mat[srow][j] << " ";
+    }
+
+    //right
+for(int i=srow+1;i<=erow;i++){
+    cout << mat[i][ecol]<< " ";
+
+}
+    //bottom
+for(int j=erow-1; j>=scol; j--){
+    cout << mat[erow][j]<< " ";
+}
+    //left
+    for(int i=erow-1; i>=srow+1; i--){
+        cout << mat[i][scol] <<  " ";
+    }
+srow++;scol++;
+erow--;ecol--;
+ }
+ cout << endl;
+}
 
 int main(){
-    int arr[3][4];
-    int n=3, m=4;
+    int mat[4][4]={{1,2,3,4},
+                      {5,6,7,8},
+                       { 9,10,11,12},
+                        { 13,14,15,16}};
+                               
+  
+spiralmatrix(mat, 4, 4);
 
-for(int i=0;i<n;i++){
-    for(int j=0;j<m;j++){
-        cin >> arr[i][j];
-    }
-
-} 
-for(int i=0;i<n;i++){
-    for(int j=0;j<m;j++){
-        cout << arr[i][j] << " ";
-    }
-    cout<< endl;
-    
-
-} 
  return 0;
 }
