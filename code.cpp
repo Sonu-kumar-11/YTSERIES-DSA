23330c792f6056f988a35b7d7ac28fd1905c42e6
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-04-18 21:53:06 +0530

    memory leak

--- code.cpp
+++ code.cpp
@@ -1,41 +1,16 @@
 #include<iostream>
-#include<string>
 using namespace std;
 
-bool isanagram(string str1, string str2 ){
-   if (str1.length() != str2.length()){
-    cout<< "not valid anagram\n";
-    return false;
-    
-   
+int* func() {
+   int *ptr =new int ;
+   *ptr = 1200;
+   cout << "ptr points to " << *ptr << endl;
+ 
+   return ptr;
 }
 
-int count[26] = {0};
-for(int i=0;i < str1.length(); i++){
-int idx = str1[i] - 'a';
-count[idx]++;
+int main () {
+   int *x = func();
+   cout << *x << endl;
+ return 0;
 }
-for(int i=0; i < str2.length(); i++){
-   int idx =str2[i] -'a';
-
-   if(count[idx]==0){
-      cout<< "not valid anagram\n";
-      return false;
-   }
-   count[idx]--;
-}
-cout << " valid anagram\n";
-return true;
-}
-
-
-int main(){
-   string str1 = " anagram";
-   string str2 = " nagaram";
-   isanagram(str1,str2);
-  return 0;
-}
-
-
-
-
